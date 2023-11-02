1449be9920ae2ad90e8589e35dee4674a16cac0e
James Brunet <james@brunet.dev>
2023-11-02 09:48:27 -0400

    add ability to create item

--- src/App.js
+++ src/App.js
@@ -14,7 +14,36 @@ class App extends Component {
     // Initialize the state of the component.
     this.state = {
       todoList: todoItems, // Stores the array of to-do items.
+      showAddItemInput: false, // Controls the visibility of the add item input.
+      newItemText: '', // Holds the current value of the add item text input.
     }
+  }
+
+  // Function to handle input text change
+  // e represents the event object that is passed to the function when the event occurs
+  handleInputChange = (e) => {
+    this.setState({ newItemText: e.target.value });
+  }
+
+  // Function to add an item
+  addItem = () => {
+    const { newItemText, todoList } = this.state;
+    const newItem = {
+      // Sets ID as a random number between 1 and 100,000
+      // If you get unlucky, two items will have the same ID
+      // For that reason, this is NOT a smart solution
+      // https://en.wikipedia.org/wiki/Birthday_problem
+      // With just 200 items, there is a 20% chance any two have the same ID!
+      id: Math.floor(Math.random() * 100000) + 1,
+      title: newItemText,
+      completed: false,
+    };
+
+    this.setState({
+      todoList: [...todoList, newItem],
+      newItemText: '', // Clear the input after adding
+      showAddItemInput: false, // Hide the input box after adding
+    });
   }
 
   renderItems = () => {
@@ -66,11 +95,48 @@ class App extends Component {
   }
 
   render() {
+    // This is the same as:
+    // const newItemText = this.state.newItemText;
+    // const todoList = this.state.todoList;
+    const { showAddItemInput, newItemText } = this.state;
+
     return (
         <main className="container">
           <div className="row">
             <div className="col-md-6 col-sm-10 mt-4 mx-auto p-0">
               <div className="card p-3">
+                <div className="mb-4">
+                  {showAddItemInput ? (
+                      <>
+                        <input
+                            type="text"
+                            className="form-control mb-2"
+                            value={newItemText}
+                            onChange={this.handleInputChange}
+                            placeholder="Enter task description"
+                        />
+                        <button
+                            className="btn btn-success"
+                            onClick={this.addItem}
+                        >
+                          Submit
+                        </button>
+                        <button
+                            className="btn btn-secondary ml-2"
+                            onClick={() => this.setState({ showAddItemInput: false })}
+                        >
+                          Cancel
+                        </button>
+                      </>
+                  ) : (
+                      <button
+                          className="btn btn-primary"
+                          onClick={() => this.setState({ showAddItemInput: true })}
+                      >
+                        Add task
+                      </button>
+                  )}
+                </div>
                 <ul className="list-group list-group-flush border-top-0">
                   {this.renderItems()}
                 </ul>
